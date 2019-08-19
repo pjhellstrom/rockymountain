28532d9bfae2436114ee0312c6cf327b0c25f64c
pjhellstrom <pjh.hellstrom@gmail.com>
2019-08-19 11:38:20 -0400

    Created models and controllers

---
+++ models/user.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  User.associate = function(models) {
+    User.hasMany(models.Note, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
